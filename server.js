1b48f3241cdd587a98086d0a35fe335d6e08d0d0
kit-foster <64404127+kit-foster@users.noreply.github.com>
2020-08-17 11:10:36 +1200

    mongo setup

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require('cors');
 
 const app = express();
 
+app.use(
+    cors(),
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+app.use(bodyParser.json());
+
+// Configure Mongo
+const db = 'mongodb://127.0.0.1:27017/seer';
+
+// Connect to Mongo with Mongoose
+mongoose
+    .connect(
+        db, 
+        { 
+            useNewUrlParser: true,
+            useUnifiedTopology: true 
+        }
+    )
+    .then(() => console.log("Mongo connected"))
+    .catch(err => console.log(err));
+
+// Specify which port and start listening
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log("listening on " + PORT);
 });
 
+// load routes
 app.get('/', (req, res) => {
     res.send({ok: true});
 });
