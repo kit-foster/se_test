15eae8a65f1b7c42a91d341ee4c0ae7b74c95327
kit-foster <64404127+kit-foster@users.noreply.github.com>
2020-08-17 16:40:50 +1200

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import './App.css';
+
+import Navbar from "./components/navbar.component"
+import CreateUser from "./components/create-user.component";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
