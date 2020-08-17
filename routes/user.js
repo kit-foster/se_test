adb4e8c69d8c031c4d38e4da4a443721b675286d
kit-foster <64404127+kit-foster@users.noreply.github.com>
2020-08-18 10:52:59 +1200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ let User = require('../schemas/user_schema');
 // find method returns a promise (in jSON format)
 router.route('/').get((req, res) => {
   User.find()
-    .then(user => res.json(user))
+    .then(user => res.json({ok: true}))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
